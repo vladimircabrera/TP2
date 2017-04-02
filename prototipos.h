79ac6fcc6eee6b1091e78e74b8f0f2c2b3cb17e6
vladimircabrera <vladimir.cv.21@gmail.com>
2017-04-02 00:23:27 -0300

    Update prototipos.h

--- prototipos.h
+++ prototipos.h
@@ -1,1 +1,9 @@
+/* PROTOTIPOS */
+double senoidal (int, int, float, int);
+double logaritmica (float);
+double loglineal (float);
+double exponencial (float);
+double escalon (float);
+double mrua (float, float, float, float);
+double paraboloide (int, int, float);
 
